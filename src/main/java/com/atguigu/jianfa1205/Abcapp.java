23fc02f2d221cf363c590a85db560dfd5b9b7f96
wysnkdxydyq <m18238649413@163.com>
2019-12-24 23:15:09 +0800

    修改1

---
+++ src/main/java/com/atguigu/jianfa1205/Abcapp.java
@@ -0,0 +1,8 @@
+package com.atguigu.jianfa1205;
+
+/**
+ * @author ycl
+ * @create 2019-12-24 23:10
+ */
+public class Abcapp {
+}
--- src/main/java/com/atguigu/jianfa1205/DuGuJiuJianApp.java
+++ src/main/java/com/atguigu/jianfa1205/DuGuJiuJianApp.java
@@ -6,6 +6,7 @@ package com.atguigu.jianfa1205;
  */
 public class DuGuJiuJianApp {
     public static void main(String[] args) {
+        System.out.println("....");
         System.out.println("dugujiujian ");
     }
 
